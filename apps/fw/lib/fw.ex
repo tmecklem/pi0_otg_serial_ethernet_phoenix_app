23c916e19b0bed69b73d6dbad7f9041d46026745
Tim Mecklem <tim@teamgaslight.com>
2017-01-20 00:05:58 -0500

    Initial commit of app that loads dhcp server on usb0 and runs a phoenix app

---
+++ apps/fw/lib/fw.ex
@@ -0,0 +1,31 @@
+defmodule Fw do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+    # Define workers and child supervisors to be supervised
+    children = [
+      supervisor(Phoenix.PubSub.PG2, [Nerves.PubSub, [poolsize: 1]]),
+      worker(Task, [fn -> start_network end], restart: :transient)
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Fw.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def start_network do
+
+    {:ok, pid} = Nerves.Networking.setup :usb0, mode: "static", ip: "192.168.73.1", router:
+      "192.168.73.1", mask: "24", subnet: "255.255.255.0", mode: "static",
+      dns: "8.8.8.8 8.8.4.4", hostname: "rpi0"
+
+    System.cmd("dnsmasq", ["--interface=usb0", "--bind-interfaces", "--dhcp-range=192.168.73.2,192.168.73.99,12h", "--no-resolv"])
+
+    {:ok, pid}
+  end
+
+end
